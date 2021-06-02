94870476b0b7a8087d4d6734b1c0a221a1a60043
kaurovaae <anechka_96@mail.ru>
2021-06-02 21:17:44 +0300

    add toggle tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,6 +4,7 @@ import {formatTweet, formatDate} from "../utils/helpers";
 import TiArrowBackOutline from 'react-icons/lib/ti/arrow-back-outline';
 import TiHeartOutline from 'react-icons/lib/ti/heart-outline';
 import TiHeartFullOutline from 'react-icons/lib/ti/heart-full-outline';
+import {handleToggleTweet} from "../actions/tweets";
 
 class Tweet extends Component {
     toParent = (e, id) => {
@@ -13,7 +14,13 @@ class Tweet extends Component {
 
     handleLike = (e) => {
         e.preventDefault();
-        //todo: Handle like Tweet
+
+        const {dispatch, tweet, authedUser} = this.props;
+        dispatch(handleToggleTweet({
+            id: tweet.id,
+            hasLiked: tweet.hasLiked,
+            authedUser
+        }))
     };
 
     render() {
